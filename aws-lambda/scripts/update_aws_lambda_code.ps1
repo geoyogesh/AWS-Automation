3d65fbae8d7448faaf80fdd8ce39ace7345d19d7
Yogesh Dhanapal <geoyogesh@gmail.com>
2018-02-13 21:20:04 -0500

    added aws lambda code publishing script

---
+++ aws-lambda/scripts/update_aws_lambda_code.ps1
@@ -0,0 +1,10 @@
+﻿$source = "C:\Work\src"
+$destination = "C:\Work\deploy\artifact.zip"
+
+# deploy artifact is made by zipping the source folder
+If(Test-path $destination) {Remove-item $destination}
+Add-Type -assembly "system.io.compression.filesystem"
+[io.compression.zipfile]::CreateFromDirectory($Source, $destination) 
+
+# source code is pushed to aws function
+aws lambda update-function-code --function-name my_lambda --zip-file fileb://C:/Work/deploy/artifact.zip
