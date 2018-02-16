f9ed6b20bf518f350e2ae39a1b385f2a63cf0f06
Yogesh Dhanapal <geoyogesh@gmail.com>
2018-02-15 22:36:12 -0500

    s3 static website script

---
+++ s3/static-website/update_s3_content.ps1
@@ -0,0 +1,13 @@
+﻿# stop execution of the script if any error occured
+$ErrorActionPreference = "Stop"
+
+
+$artifact_extract_folder = ""
+$s3_bucket = ''
+$aws_profile = ''
+$cloudfront_distribution_id = ''
+
+
+aws s3 sync "$artifact_extract_folder" s3://$s3_bucket/ --delete --only-show-errors --profile $aws_profile
+# aws s3 cp "$artifact_extract_folder" s3://$s3_bucket/ --recursive --profile $aws_profile
+# aws cloudfront create-invalidation --distribution-id $cloudfront_distribution_id --path '/*' --profile $aws_profile
